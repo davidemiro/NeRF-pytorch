f8a2cd057fc8d391ff6805f3c4d27d9dd9154389
mirodavide <filomenodavide.miro@gmail.com>
2024-02-10 00:06:13 +0100

    refactoring encoding

--- nerf/create_nerf.py
+++ nerf/create_nerf.py
@@ -3,7 +3,7 @@ from nerf.model import MLP
 from nerf.encoding import PositionalEncoder
 
 
-def init_nerf():
+def init_nerf(args):
     """
     :return: The coarse and fine Multi Layer Perceptrons and the Positional encodings for the spatial coordinates and the views.
     """
@@ -11,9 +11,10 @@ def init_nerf():
 
     coarse_mlp = MLP()
     fine_mlp = MLP()
-
-    embedding_pts = PositionalEncoder(L=10)
-    embedding_views = PositionalEncoder(L=4)
+    if args.L > 0:
+        embedding_pts = PositionalEncoder(L=10)
+    if args.with_views and args.L_view > 0:
+        embedding_views = PositionalEncoder(L=4)
 
     return coarse_mlp,fine_mlp,embedding_pts,embedding_views
 
--- nerf/encoding.py
+++ nerf/encoding.py
@@ -5,16 +5,34 @@ class PositionalEncoder(nn.Module):
     """
     This torch Module define the positional encoding strategy as described in the NeRF official paper
     """
-    def __init__(self,L):
-        super(PositionalEncoder, self).__init__()
-        self.L = L
-        self.freqs = torch.linspace(2**0,2**(L-1),L)
-        self.encoding_functions = []
+    def __init__(self,l ,log_sampling=True, include_input=True, embed=True):
+        """
 
-        for freq in self.freqs:
-            self.encoding_functions.append(lambda x: torch.cos(freq*torch.pi*x))
-            self.encoding_functions.append(lambda x: torch.sin(freq*torch.pi*x))
+        :param l:
+        :param log_sampling:
+        :param embed:
+        """
+        super(PositionalEncoder, self).__init__()
+        self.l = l
+
+        if log_sampling:
+            self.frequencies = 2.**torch.linspace(0., l - 1, l)
+        else:
+            self.frequencies = torch.linspace(2**0, 2**(l-1), l)
+
+        self.encoding_functions = []
+        if embed:
+            self.functions = [lambda x: torch.cos(x), lambda x: torch.sin(x)]
+            if include_input:
+                self.functions.append(lambda x: x)
+        else:
+            self.functions = [lambda x: x]
+
+        for freq in self.frequencies:
+            for function in self.functions:
+                self.encoding_functions.append(lambda x: function(x*freq*torch.pi))
 
     def forward(self,x):
+
         #Apply positional encoding to the input x -> [cos(2**0*pi*x),sin(2**0*pi*x),...,cos(2**(L-1)*pi*x),sin(2**(L-1)*pi*x)]
         return torch.cat([encoding_function(x) for encoding_function in self.encoding_functions],dim=-1)
